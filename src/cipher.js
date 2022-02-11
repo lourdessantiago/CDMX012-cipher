0efb9017502591122d302ea29b67147c7bd1c5d5
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-02-10 22:21:47 -0600

    primera version sin descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,216 @@
-const cipher = {
-  // ...
-};
+/* const fnArrow = (x) => {
+  console.log(x.toLowerCase());
+  return x.toLowerCase();
+}; flecha 
 
+() => {}
+
+(parametros) => { codigo ... }
+
+const fnAnonymus = function (x) {}; aninima 
+
+function conNombre (x) {} con nombre*/
+
+const limInfMayus = 65;
+const limSupMayus = 90;
+const limInfMinus = 97;
+const limSupMinus = 122;
+
+function desbordeDerMayus(offset, valorLetra) {
+  const nuevoValor = offset + valorLetra;
+  if (nuevoValor < limSupMayus) {
+    return nuevoValor;
+  }
+
+  const desborde = offset % (limSupMayus - limInfMayus);
+  const valor = (valorLetra + desborde) - 1;
+  if (valor < limSupMayus) {
+    return valor;
+  }
+  const valor2 = (limInfMayus - 1) + (valor - limSupMayus);
+  const desbordeIzq = valor2 < limInfMayus;
+  if (desbordeIzq) {
+    return limSupMayus;
+  }
+  else {
+    return valor2;
+  }
+}
+
+function desbordeDerMinus(offset, valorLetra) {
+  const nuevoValor = offset + valorLetra;
+  if (nuevoValor < limSupMinus) {
+    return nuevoValor;
+  }
+
+  const desborde = offset % (limSupMinus - limInfMinus);
+  const valor = (valorLetra + desborde) - 1;
+  if (valor < limSupMinus) {
+    return valor;
+  }
+  const valor2 = (limInfMinus - 1) + (valor - limSupMinus);
+  const desbordeIzq = valor2 < limInfMinus;
+  if (desbordeIzq) {
+    return limSupMinus;
+  }
+  else {
+    return valor2;
+  }
+}
+
+
+
+
+/**
+ * obtener el nuevo valor, offset va a caminar hacia la izquierda por lo tanto es una resta.  
+ *  ver que el nuevo valor no sea menor al liminte inferior pero si es mayor o = entonces retornar nuevo valor
+ * sacer desborde
+ * si desborde es mayor al mlimite entonces se pondra valor 
+ * 
+ */
+
+
+/* function desbordeIzqMayus(offset, valorletra) {
+  const nuevoValor = offset + valorletra;
+  if (nuevoValor >= limInfMayus) {
+    return nuevoValor;
+  }
+
+  const desborde = offset % (limInfMayus - limSupMayus);
+  const valor = (valorletra + desborde) - 1;
+  if (valor >= limInfMayus) {
+    return valor;
+  }
+  const valor2 = (limSupMayus - 1) + (valor - limSupMayus);
+  const desbordeIzq = valor2 >= limSupMinus;
+  if (desbordeIzq) {
+    return limInfMayus;
+  }
+  else {
+    return valor2;
+  }
+} */
+
+
+
+
+function encode(offset, string) {
+  //console.log(offset, string)
+  const palabras = string.trim().split(' ');
+  //console.log(palabras)
+  //console.log(offset.value);
+  let mensajeCifrado = [];
+  for (const palabra of palabras) {
+    let palabraCifrada = '';
+    for (const letra of palabra) {
+      palabraCifrada += cifrarLetra(letra, + offset);
+    }
+
+    mensajeCifrado.push(palabraCifrada);
+  }
+  //console.log(mensajeCifrado);
+  // console.log(mensajeCifrado.join(' '));
+
+  return mensajeCifrado.join(' ');
+}
+
+function decode(offset, string) {
+  //console.log(offset, string)
+  const palabras = string.trim().split(' ');
+  //console.log(palabras)
+  //console.log(offset.value);
+  let mensajeCifrado = [];
+  for (const palabra of palabras) {
+    let palabraCifrada = '';
+    for (const letra of palabra) {
+      palabraCifrada += cifrarLetra(letra, - offset);
+    }
+    mensajeCifrado.push(palabraCifrada);
+  }
+  //console.log(mensajeCifrado);
+  // console.log(mensajeCifrado.join(' '));
+  return mensajeCifrado.join(' ');
+
+}
+
+
+
+
+
+function cifrarLetra(letra, offset) {
+
+  const valorAscii = letra.charCodeAt();
+
+
+  if (valorAscii >= 65 && valorAscii <= 90) {
+    const valorLetra = desbordeDerMayus(offset, valorAscii);
+
+    return String.fromCharCode(valorLetra);
+  }
+
+  if (valorAscii >= 97 && valorAscii <= 122) {
+    const valorLetraMinus = desbordeDerMinus(offset, valorAscii);
+    
+    return String.fromCharCode(valorLetraMinus);
+  
+  }
+ 
+  return letra;
+
+}
+
+
+
+
+function descifrarLetra(letra, offset) {
+  
+  const valorAscii = letra.charCodeAt();
+
+  if (valorAscii >= 65 && valorAscii <= 90) {
+
+    const valorLetra = (offset, valorAscii)
+
+    return String.fromCharCode(valorLetra - offset);
+  }
+  if (valorAscii >= 122 && valorAscii <= 97) {
+    return String.fromCharCode(valorAscii - offset);
+  }
+
+  return letra;
+
+}
+
+/* function desbordeIzqMayus(offset, valorLetra) {
+  const nuevoValor = offset + valorLetra;
+  if (nuevoValor < limSupMinus) {
+    return nuevoValor;
+  }
+
+  const desborde = offset % (limSupMinus - limInfMinus);
+  const valor = (valorLetra + desborde) - 1;
+  if (valor < limSupMinus) {
+    return valor;
+  }
+  const valor2 = (limInfMinus - 1) + (valor - limSupMinus);
+  const desbordeIzq = valor2 < limInfMinus;
+  if (desbordeIzq) {
+    return limSupMinus;
+  }
+  else {
+    return valor2;
+  }
+} */
+
+
+
+// cifrarTexto: (nombre) => "Mi nombre es:"+ nombre,
+// ...
+const cipher = {
+  cifrarLetra, descifrarLetra, encode, decode,
+} // export default cipher;
 export default cipher;
+
+//const letraCifrada= String.fromCharCode(valorAscii+1)
+// return `la letra es: ${letra} => ${letraCifrada}`
+//console.log(typeof saltos)
+//return String.fromCharCode(valorAscii + offset);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const buttonCifrar = document.querySelector('#buttonCifrar');
+
+const buttonDescifrar = document.querySelector('#buttonDescifrar');
+
+const mensaje = document.querySelector('#cifrarMensaje');
+
+const mensajeListo = document.querySelector('#mensajeListo');
+
+const offset = document.querySelector('#offset');
+
+
+buttonCifrar.addEventListener('click', () => {
+   
+   const mensajePrueba = cipher.encode (parseInt(offset.value),mensaje.value.trim());
+   mensajeListo.value = mensajePrueba;
+});
+
+buttonDescifrar.addEventListener('click', () => {
+    const mensajePrueba =cipher.decode (parseInt(offset.value),mensaje.value.trim());
+    mensajeListo.value =mensajePrueba;
+    
+    /* const palabras = encode.value.trim().split(' ');
+    let mensajeDescifrado = [];
+    for (const palabra of palabras) {
+        let palabraDescifrada = '';
+        for (const letra of palabra) {
+            palabraDescifrada += cipher.descifrarLetra(letra, parseInt(offset.value));
+
+        }
+        mensajeDescifrado.push(palabraDescifrada);
+
+    }
+    mensajeListo.value = mensajeDescifrado.join(' '); */
+});
+
+// obtener valor de la caja listo
+// obtener la palabras, que separe las palabras de los espacios (arreglo de las palabras que tenga el mensaje ) listo
+// cifrar cada palabra
+
+
+
+
+
+
+
+
+
+// selector
+// selector de id
+// variable
+// query selecto
+// investigar adddeventlistener (clic)
+// como trabajan las funciones
+
